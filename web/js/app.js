f40af531199662e48a4b9ee4c4ca40fc779da866
David Gomez <david.ernesto.gomez@correounivalle.edu.co>
2016-01-16 17:05:56 -0500

    aniado emojis en fb description.... tal vez funcionen

--- web/js/app.js
+++ web/js/app.js
@@ -136,7 +136,7 @@ window.onload = function() {
   {
         console.log('http://appsancocho.herokuapp.com'+friendCache.savedImage);
            FB.api('/me/photos', 'post', {
-            message:'#VamosPalSancocho #SancochoFest2016 , creado con https://apps.facebook.com/vamospalsancocho/',
+            message:'#VamosPalSancocho #SancochoFest2016 , :cry: :zzz: creado con https://apps.facebook.com/vamospalsancocho/',
             url:'http://appsancocho.herokuapp.com'+friendCache.savedImage       
         
         }, function(response){
